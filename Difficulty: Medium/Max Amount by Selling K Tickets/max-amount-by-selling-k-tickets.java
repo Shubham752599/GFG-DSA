e5ea96227e7c2734cb16e87eb154d62daba9a01a
Shubham Maurya <shubhamkushwaha7525@gmail.com>
2026-07-12 23:37:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Amount by Selling K Tickets/max-amount-by-selling-k-tickets.java
@@ -0,0 +1,25 @@
+class Solution {
+    static final int mod = 1000000007;
+  
+    public int maxAmount(int[] arr, int k) {
+        // code here
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int tickets:arr){
+            pq.offer(tickets);
+            
+        }
+        long ans = 0;
+        while(k > 0 && !pq.isEmpty()){
+            int maxTicket = pq.poll();
+            ans= (ans + maxTicket)% mod;
+            maxTicket--;
+            if(maxTicket>0){
+                pq.offer(maxTicket);
+                
+            }
+            k--;
+        }
+        return (int)ans;
+        }
+};
