eff833c6ce028ae3e46604d64fd2041825796033
Shubham Maurya <shubhamkushwaha7525@gmail.com>
2026-08-02 20:25:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sequences where Adjacent Divide/sequences-where-adjacent-divide.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int count(int n, int m) {
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= m; i++) {
+            dp[1][i] = 1;
+        }
+        for (int len = 2; len <= n; len++) {
+            for (int last = 1; last <= m; last++) {
+                int ways = 0;
+                for (int prev = 1; prev <= m; prev++) {
+                    if (prev % last == 0 || last % prev == 0) {
+                        ways += dp[len - 1][prev];
+                    }
+                }
+                dp[len][last] = ways;
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i <= m; i++) {
+            ans += dp[n][i];
+        }
+        return ans;
+    }
+}
